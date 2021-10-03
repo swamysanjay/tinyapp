ab04baac7ed8c2e59fd41b5b4e6ac81521c61d0e
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-03 18:45:51 +0000

    added a directory for short URLs

--- express_server.js
+++ express_server.js
@@ -18,6 +18,13 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: /* What goes here? */ };
+  res.render("urls_show", templateVars);
+});
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
