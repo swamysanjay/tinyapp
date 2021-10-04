0e019eda4940e852348e495c7fcf1e149f9caffd
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-04 06:34:35 +0000

    Created a mocha and chai test that passes both test cases

---
+++ test/helpersTest.js
@@ -0,0 +1,27 @@
+const { assert } = require('chai');
+const { getUserByEmail } = require('../helpers.js');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('guetUserByEmail', function() {
+  it('should return a user with valid email', function() {
+    const user = getUserByEmail("user2@example.com", testUsers);
+    const expectedOutput = testUsers.user2RandomID;
+    assert.equal(user, expectedOutput);
+  });
+  it('should return undefined with an invalid email', function() {
+    const user = getUserByEmail("user3@example.com", testUsers);
+    assert.equal(user, undefined);
+  });
+});
