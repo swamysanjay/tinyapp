0d844b61be07091083c9c9a913a6e7fce283c5b9
Sanjay Swamy <swamysanjay98@gmail.com>
2021-10-04 06:24:30 +0000

    Moved all helper functions into helpers.js

--- express_server.js
+++ express_server.js
@@ -4,7 +4,7 @@ const PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
 const bcrypt = require('bcryptjs');
 const cookieSession = require('cookie-session');
-const {getUserByEmail} = require('./helpers');
+const {getUserByEmail, generateRandomString, urlsForUser, addUser} = require('./helpers');
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(
@@ -41,42 +41,6 @@ const users = {
     password: "dishwasher-funk"
   }
 }
-
-//Functions
-
-const addUser = (email, password) => {
-  const hashPass = bcrypt.hashSync(password, 10);
-  const id = generateRandomString();
-  users[id] = {
-    id,
-    email,
-    password: hashPass
-  };
-  return id;
-}
-
-const urlsForUser = (id) => {
-  let filter = {};
-  for (let urlID of Object.keys(urlDatabase)) {
-    if (urlDatabase[urlID].userID === id) {
-      filter[urlID] = urlDatabase[urlID];
-    }
-  }
-  return filter;
-}
-
-const generateRandomString = function() {
-  const array = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
-    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d",
-    "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
-    "v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  let randString = "";
-  while (randString.length < 6) {
-    let index = Math.floor(Math.random() * 61);
-    randString += array[index];
-  }
-  return randString;
-};
 
 //Routes
 
--- helpers.js
+++ helpers.js
@@ -1,5 +1,43 @@
+const bcrypt = require('bcryptjs');
+const urlDatabase = require('./express_server');
+const users = require('./express_server');
+
 const getUserByEmail = (email, database) => {
   return Object.values(database).find(user => user.email === email);
 }
 
-module.exports = {getUserByEmail};
+const addUser = (email, password) => {
+  const hashPass = bcrypt.hashSync(password, 10);
+  const id = generateRandomString();
+  users[id] = {
+    id,
+    email,
+    password: hashPass
+  };
+  return id;
+}
+
+const urlsForUser = (id) => {
+  let filter = {};
+  for (let urlID of Object.keys(urlDatabase)) {
+    if (urlDatabase[urlID].userID === id) {
+      filter[urlID] = urlDatabase[urlID];
+    }
+  }
+  return filter;
+}
+
+const generateRandomString = function() {
+  const array = ["A","B","C","D","E","F","G","H","I","J","K","L","M",
+    "N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d",
+    "e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u",
+    "v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  let randString = "";
+  while (randString.length < 6) {
+    let index = Math.floor(Math.random() * 61);
+    randString += array[index];
+  }
+  return randString;
+};
+
+module.exports = {getUserByEmail, addUser, urlsForUser, generateRandomString};
